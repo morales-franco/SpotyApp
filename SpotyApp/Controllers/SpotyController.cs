0c2b5d972aa57b4a52e85eacb9a35ae68c5ba4c1
Franco Morales <franco.e.morales@gmail.com>
2019-01-23 21:26:34 -0300

    upload

---
+++ SpotyApp/Controllers/SpotyController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SpotyApp.Services.Interfaces;
+
+namespace SpotyApp.Controllers
+{
+    public class SpotyController : Controller
+    {
+        private readonly ISpotyService _spotyService;
+
+        public SpotyController(ISpotyService service)
+        {
+            _spotyService = service;
+        }
+        public IActionResult Index()
+        {
+            var token = _spotyService.GetToken();
+
+            return View();
+        }
+    }
+}
---
+++ SpotyApp/Services/SpotyService.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Extensions.Configuration;
+using SpotyApp.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace SpotyApp.Services
+{
+    public class SpotyService : ISpotyService
+    {
+        private readonly IConfiguration _configuration;
+
+        public SpotyService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public string GetToken()
+        {
+            HttpClient httpClient = new HttpClient();
+            //httpClient.BaseAddress = new Uri("https://api.github.com/");
+            //// GitHub API versioning
+            //httpClient.DefaultRequestHeaders.Add("Accept",
+            //    "application/vnd.github.v3+json");
+            //// GitHub requires a user-agent
+            //httpClient.DefaultRequestHeaders.Add("User-Agent",
+            //    "HttpClientFactory-Sample");
+
+            //var response = httpClient.GetAsync(
+            //"/repos/aspnet/docs/issues?state=open&sort=created&direction=desc").Result;
+
+            //response.EnsureSuccessStatusCode();
+
+    
+
+            var clientId = _configuration["Spotify:ClientId"];
+            var clientSecret = _configuration["Spotify:ClientSecret"];
+
+            httpClient.BaseAddress = new Uri("https://accounts.spotify.com/api/");
+            var response = httpClient.GetAsync("/token").Result;
+
+            return clientId;
+        }
+    }
+}
